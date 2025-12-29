914957cc41d61f86362f8bb3186e3fc7799cb743
Hariprasad G <harig@Hariprasads-Laptop.local>
2025-12-29 18:41:16 +0530

    Reduce sponsors carousel speed to 5 seconds

--- src/components/SponsorsSection.tsx
+++ src/components/SponsorsSection.tsx
@@ -43,7 +43,7 @@ export function SponsorsSection({ sponsors }: SponsorsSectionProps) {
             style={{
               animation: isPaused
                 ? "none"
-                : "scroll 15s linear infinite",
+                : "scroll 5s linear infinite",
             }}
           >
             {duplicatedSponsors.map((sponsor, index) => (
